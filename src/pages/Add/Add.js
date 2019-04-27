4830750c187082b3dca41edec8f82d13d030b8ce
deeem <karpov.dimon@gmail.com>
2019-04-27 10:47:35 +0300

    add form markup

--- src/pages/Add/Add.js
+++ src/pages/Add/Add.js
@@ -1,7 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
+import classes from './Add.module.css';
+import Input from '../../components/UI/Input/Input';
+import Button from '../../components/UI/Button/Button';
 
-const add = (props) => (
-    <h1>Add Bit Page</h1>
-);
+class Add extends Component {
+    state = {
+        form: {
+            title: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Bit Title',
+                },
+                value: '',
+            },
+            description: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Description',
+                },
+                value: '',
+            },
+            players: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                        { value: 1, displayValue: '1' },
+                        { value: 2, displayValue: '2' },
+                    ]
+                },
+                value: '',
+            }
+        }
+    }
 
-export default add;
+    render() {
+
+        const formElementsArray = [];
+        for (let key in this.state.form) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.form[key]
+            })
+        }
+
+        let form = (
+            <form>
+                {formElementsArray.map(formElement => (
+                    <Input
+                        key={formElement.id}
+                        elementType={formElement.config.elementType}
+                        elementConfig={formElement.config.elementConfig}
+                        value={formElement.config.value}
+                    />
+                ))}
+                <Button btnType="Success">Add new Bit</Button>
+            </form>
+        );
+
+        return (
+            <div className={classes.Add}>
+                <h1>Add new Bit</h1>
+                {form}
+            </div>
+        )
+    }
+}
+
+export default Add;

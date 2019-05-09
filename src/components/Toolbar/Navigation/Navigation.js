9dd8222ad5564adf6d7cab625ea01c49891dfa7a
deeem <karpov.dimon@gmail.com>
2019-05-09 17:10:09 +0300

    hide add page from unauth user

--- src/components/Toolbar/Navigation/Navigation.js
+++ src/components/Toolbar/Navigation/Navigation.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import classes from './Navigation.module.css';
 
-const navigation = () => {
-    return (
-        <ul className={classes.NavigationItems}>
-            <NavLink exact to='/' activeClassName={classes.active} className={classes.NavigationItem}>Browse</NavLink>
-            <NavLink exact to='/random' activeClassName={classes.active} className={classes.NavigationItem}>Random</NavLink>
-            <NavLink exact to='/add' activeClassName={classes.active} className={classes.NavigationItem}>Add</NavLink>
-            <NavLink exact to='/auth' activeClassName={classes.active} className={classes.NavigationItem}>Auth</NavLink>
-        </ul>
-    );
+class Navigation extends Component {
+
+    render() {
+        return (
+            <ul className={classes.NavigationItems}>
+                <NavLink exact to='/' activeClassName={classes.active} className={classes.NavigationItem}>Browse</NavLink>
+                <NavLink exact to='/random' activeClassName={classes.active} className={classes.NavigationItem}>Random</NavLink>
+                {this.props.isAuthenticated ? <NavLink exact to='/add' activeClassName={classes.active} className={classes.NavigationItem}>Add</NavLink> : null}
+                <NavLink exact to='/auth' activeClassName={classes.active} className={classes.NavigationItem}>Auth</NavLink>
+            </ul>
+        )
+    }
 }
 
-export default navigation;
+const mapStateToProps = state => {
+    return {
+        isAuthenticated: state.auth.token !== null,
+    }
+}
+
+export default connect(mapStateToProps)(Navigation);

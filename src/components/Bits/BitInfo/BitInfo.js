2201fd5efca9c92860c9eeb6466f155a87ea6e6c
deeem <karpov.dimon@gmail.com>
2019-06-02 15:54:26 +0300

    bitinfo basic styling

--- src/components/Bits/BitInfo/BitInfo.js
+++ src/components/Bits/BitInfo/BitInfo.js
@@ -3,13 +3,26 @@ import classes from './BitInfo.module.css';
 import Button from '../../UI/Button/Button';
 
 const bitInfo = (props) => (
-    <div className={classes['BitModal-container']}>
-        <h1>{props.bit.game.title}</h1>
-        <img src={unescape(props.bit.game.image)} />
-        <p>players: {props.bit.players}</p>
-        <p>difficult: {props.bit.difficult}</p>
-        <p>rating: {props.bit.rating}</p>
+    <div className={classes['bitinfo']}>
+
+        <img className={classes['bitinfo__image']} src={unescape(props.bit.game.image)} />
+
+        <h2 className={classes['bitinfo__title']}>{props.bit.title}</h2>
+
+        <section className={classes['bitinfo__bit-description']}>
+            <h3>Bit Description</h3>
+            <p>{props.bit.game.description}</p>
+        </section>
+
+
+        <section className={classes['bitinfo__badges']}>
+            <p>players: {props.bit.players}</p>
+            <p>difficult: {props.bit.difficult}</p>
+            <p>rating: {props.bit.rating}</p>
+        </section>
+
         <Button clicked={props.hide}>Close</Button>
+
     </div>
 );
 

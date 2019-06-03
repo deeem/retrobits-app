57982bff621345f346f5670bf9b311074c7c538e
deeem <karpov.dimon@gmail.com>
2019-06-03 16:05:17 +0300

    update bit images

--- src/components/Bits/BitListItem/BitListItem.js
+++ src/components/Bits/BitListItem/BitListItem.js
@@ -4,12 +4,14 @@ import './BitListItem.css';
 const BitListItem = (props) => {
     return (
         <div class="bit">
-            <div class="bit__image"></div>
-
+            <img className='bit__image' src={unescape(props.data.game.image)} />
             <h3 class="bit-info__bit-title">{props.data.title}</h3>
-            <p class="bit-info__game-title">{props.data.game.title}</p>
-            <span class="bit-info__bit-difficult">normal</span>
-            <span class="bit-info__bit-author">normal</span>
+            <p class="bit-info__game-title">
+                {props.data.game.title}
+                <span className="bit-info__game-platform">
+                    {props.data.game.platform.title}
+                </span>
+            </p>
             <button onClick={props.click}>Show Bit</button>
         </div>
     )
